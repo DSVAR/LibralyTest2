f83d5f9f8756a8fa39737ae0b980f56dfa5b966a
DVAR <danilka070103@gmail.com>
2021-01-14 07:04:24 +0300

    переписать контроллер

---
+++ Libraly(test2)/Areas/Account/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libraly_test2_.Areas.Account
+{
+    public class AccountController : Controller
+    {
+
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
